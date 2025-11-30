0a31289088a9b30685f9756ad568c31a9f784ef2
Favour Evans <divfavour04@gmail.com>
2025-11-30 01:49:29 +0100

    added react phone input style import

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import 'react-phone-number-input/style.css'
 import "./index.css";
 import App from "./App.jsx";
 
