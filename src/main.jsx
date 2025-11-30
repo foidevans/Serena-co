e8643b89ab7c1c104f4ad14bcab464c4b030e598
Favour Evans <divfavour04@gmail.com>
2025-11-30 02:02:13 +0100

    added react input style in assets

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import 'react-phone-number-input/style.css'
+// import 'react-phone-number-input/style.css'
+import './assets/css/style.css';
+// import './assets/css/'
 import "./index.css";
 import App from "./App.jsx";
 
